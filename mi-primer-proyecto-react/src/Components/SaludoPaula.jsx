1a4a6e479f97a038e4a8fd1ba6bcb4b08b6d7d87
Maria Paula Pérez Garcia <paulapgarciazzz@gmail.com>
2026-03-26 18:39:04 -0600

    crear componentes

---
+++ mi-primer-proyecto-react/src/Components/SaludoPaula.jsx
@@ -0,0 +1,15 @@
+import {useState} from 'react';
+import './SaludoPaula.css';
+
+export default function SaludoPaula() {
+    const [name, setName] = useState('Paula');  
+
+    return (
+        <section className='Saludo'>
+            <h2 className='saludo_title'>¡Hola {name}!    </h2>
+            <p className='saludo_extra'>
+            ¡buenas!    
+            </p>
+        </section>
+    )
+}
